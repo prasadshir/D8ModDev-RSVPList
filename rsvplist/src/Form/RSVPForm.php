cc866a4675aef9b6b27ac11087d925f7ddb1f0f7
Prasa Shirgaonkar <prasad.shirgaonkar@MacBook-Air-7.local>
2017-03-03 18:48:21 +0530

    changed DB insert query in RSVPForm.php

--- rsvplist/src/Form/RSVPForm.php
+++ rsvplist/src/Form/RSVPForm.php
@@ -1,21 +1,20 @@
 <?php
-/**
- * @file
- * Contains \Drupal\rsvplist\Form\RSVPForm
- */
+
 namespace Drupal\rsvplist\Form;
 
 use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
-/**
- * Provides a RSVP Email form.
- */
-class RSVPForm extends FormBase {
 
+/**
+ * Provides an RSVP Email form
+ * */
+
+class RSVPForm extends FormBase {
   /**
-   * {@inheritdoc}
+   * (@inheritdoc)
+   *
    */
   public function getFormId() {
     return 'rsvplist_email_form';
@@ -54,20 +53,30 @@ class RSVPForm extends FormBase {
       $form_state->setErrorByName('email', t('The email address %mail is not valid.', array('%mail' => $value)));
       return;
     }
-    
- /**
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
-    db_insert('rsvplist')
-      ->fields(array(
-        'mail' => $form_state->getValue('email'),
-        'nid' => $form_state->getValue('nid'),
-        'uid' => $user->id(),
-        'created' => time(),
-       ))
-      ->execute();
+
+    $query = \Drupal::database()->insert('rsvplist');
+    $query->fields([
+      'mail',
+      'nid',
+      'uid',
+      'created',
+    ]);
+    $query->values(
+      [ $form_state->getValue('email'),
+        $form_state->getValue('nid'),
+        $user->id(),
+        time(),
+      ]
+    );
+    $query->execute();
+
     drupal_set_message(t('Thank you for your RSVP, you are on the list for the event.'));
   }
 }
