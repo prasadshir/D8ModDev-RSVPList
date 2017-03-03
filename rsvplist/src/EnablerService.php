2349987d9d96be8bac2b2e39b3731f8edf2e314c
Prasa Shirgaonkar <prasad.shirgaonkar@MacBook-Air-7.local>
2017-03-03 11:43:56 +0530

    3.10.1

---
+++ rsvplist/src/EnablerService.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\rsvplist\EnablerService.
+ */
+
+namespace Drupal\rsvplist;
+
+use Drupal\Core\Database\Database;
+use Drupal\node\Entity\Node;
+
+/**
+ * Defines a service for managing RSVP list enabled for nodes.
+ */
+class EnablerService {
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+}
