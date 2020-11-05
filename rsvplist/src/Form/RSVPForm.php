a79a50c1116f67eec62ae5b3235a7263bc883e7b
Prasad Shirgaonkar <prasad.shirgaonkar@acquia.com>
2020-11-05 23:37:46 +0530

    changed 3101

--- rsvplist/src/Form/RSVPForm.php
+++ rsvplist/src/Form/RSVPForm.php
@@ -1,46 +1,45 @@
 <?php
+/**
+ * @file
+ * Contains \Drupal\rsvplist\Form\RSVPForm
+ */
 
-namespace Drupal\rsvplist\Form;
+ namespace Drupal\rsvplist\Form;
 
 use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-
+use Drupal\Core\Messenger\MessengerInterface;
 
 /**
- * Provides an RSVP Email form
- * */
-
+ * Provides a RSVP Email form.
+ */
 class RSVPForm extends FormBase {
-  /**
-   * (@inheritdoc)
-   *
-   */
-  public function getFormId() {
-    return 'rsvplist_email_form';
-  }
 
   /**
    * {@inheritdoc}
    */
+  public function getFormId() {
+    return 'rsvplist_email_form';
+  }
   public function buildForm(array $form, FormStateInterface $form_state) {
     $node = \Drupal::routeMatch()->getParameter('node');
     $nid = $node->nid->value;
-    $form['email'] = array(
+    $form['email'] = [
       '#title' => t('Email address'),
       '#type' => 'textfield',
       '#size' => 25,
       '#description' => t("We'll send updates to the email address you provide."),
       '#required' => TRUE,
-    );
-    $form['submit'] = array(
+    ];
+    $form['submit'] = [
       '#type' => 'submit',
       '#value' => t('RSVP'),
-    );
-    $form['nid'] = array(
+    ];
+    $form['nid'] = [
       '#type' => 'hidden',
       '#value' => $nid,
-    );
+    ];
     return $form;
   }
 
@@ -49,8 +48,8 @@ class RSVPForm extends FormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $value = $form_state->getValue('email');
-    if ($value == !\Drupal::service('email.validator')->isValid($value)) {
-      $form_state->setErrorByName('email', t('The email address %mail is not valid.', array('%mail' => $value)));
+    if (!(\Drupal::service('email.validator')->isValid($value))) {
+      $form_state->setErrorByName('email', $this->t('The email address %mail is not valid.', ['%mail' => $value]));
       return;
     }
   }
@@ -60,23 +59,21 @@ class RSVPForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
-
     $query = \Drupal::database()->insert('rsvplist');
-    $query->fields(array(
+    $query->fields([
       'mail',
       'nid',
       'uid',
       'created',
-    ));
-    $query->values(array(
-        $form_state->getValue('email'),
+    ]);
+    $query->values(
+      [ $form_state->getValue('email'),
         $form_state->getValue('nid'),
         $user->id(),
         time(),
-      )
+      ]
     );
     $query->execute();
-
-    drupal_set_message(t('Thank you for your RSVP, you are on the list for the event.'));
+    $this->messenger()->addMessage(t(('Thank you for your RSVP, you are on the list for the event.')));
   }
 }
