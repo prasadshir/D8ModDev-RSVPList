8c6493dd9eedf6ad7193ee161899b652254f4a84
Prasad Shirgaonkar <prasad.shirgaonkar@acquia.com>
2020-11-24 13:30:11 +0530

    removed deprecated line

--- rsvplist/src/Form/RSVPSettingsForm.php
+++ rsvplist/src/Form/RSVPSettingsForm.php
@@ -39,7 +39,6 @@ class RSVPSettingsForm extends ConfigFormBase {
       '#options' => $types,
       '#description' => $this->t('On the specified node types, an RSVP option will be available and can be enabled while tht node is being edited.'),
     ];
-    $form['array_filter'] = ['#type' => 'value', '#value' => TRUE];
 
     return parent::buildForm($form,$form_state);
   }
